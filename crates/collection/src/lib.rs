4d8e4faf238b97649864e497fea643c2acc742a2
gregortokarev <e@tokarev.work>
2026-07-27 09:19:23 +0300

    feat(collection): add collection registry

--- crates/collection/src/lib.rs
+++ crates/collection/src/lib.rs
@@ -1,7 +1,9 @@
 mod collection;
 mod entry;
+mod registry;
 mod request;
 
 pub use collection::{Collection, CollectionLoadError, CollectionSaveError};
 pub use entry::{DirEntry, Entry, FileEntry};
+pub use registry::{CollectionRegistry, CollectionRegistryLoadError};
 pub use request::{HttpRequest, Method, Request};
